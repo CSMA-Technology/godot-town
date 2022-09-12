9d739ad170f135951a2cb19b7db90e00ef894cc4
Stephanie Cruz <exscruzme@gmail.com>
2022-09-12 12:47:09 -0400

    added navigation to cars

--- Movable.gd
+++ Movable.gd
@@ -4,6 +4,9 @@ export (String) var goal # Our scene unique name
 onready var goal_pos = get_node(goal).global_position
 export var speed = 50 # The speed we should move at (pixels per second)
 export var arrival_tolerance = 10 # How close to a point do we get before we are there
+export (int, LAYERS_2D_NAVIGATION) var nav_layer = 1
+var path = null
+var path_idx = 0
 
 func _physics_process(delta):
 	# When we reach our goal (within tolerance), queue_free
@@ -14,9 +17,17 @@ func _physics_process(delta):
 		move_towards_goal(delta * speed)
 
 func move_towards_goal(speed):
-	# Get a unit-length vector pointing in the direction we want to go
-	var direction = (goal_pos - global_position).normalized()
-	# Apply our speed to the direction vector to get our movement vector
+	# If the path is null or empty, create it and return. 
+	if not path:
+		# map_get_path returns an array of points, ending at the goal if it is reachable
+		path = Navigation2DServer.map_get_path(get_world_2d().navigation_map, global_position, goal_pos, false, nav_layer)
+		# This will point to our next target along the path
+		path_idx = 0
+		return
+	# We are using the same tolerance as before to determine if we have arrived at an intermediate point along the path
+	while global_position.distance_to(path[path_idx]) <= arrival_tolerance:
+		path_idx += 1
+	# Same logic as before, but this time going to the next point in the path instead of the goal
+	var direction = (path[path_idx] - global_position).normalized()
 	var movement = direction * speed
-	# Move our KinematicBody2D node
 	move_and_collide(movement)
