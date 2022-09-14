9ec20662c71b6e6085b3d258896bd24a68b0fd18
Stephanie Cruz <exscruzme@gmail.com>
2022-09-14 10:14:42 -0400

    fixed road tiles, renamed variable

--- Movable.gd
+++ Movable.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 export (String) var goal # Our scene unique name
 onready var goal_pos = get_node(goal).global_position
-export var speed = 50 # The speed we should move at (pixels per second)
+export var current_speed = 50 # The speed we should move at (pixels per second)
 export var arrival_tolerance = 10 # How close to a point do we get before we are there
 
 func _physics_process(delta):
@@ -11,12 +11,12 @@ func _physics_process(delta):
 		queue_free()
 	# If we are not at our goal, keep moving!
 	else:
-		move_towards_goal(delta * speed)
+		move_towards_goal(delta * current_speed)
 
-func move_towards_goal(current_speed): 
+func move_towards_goal(speed): 
 	# Get a unit-length vector pointing in the direction we want to go
 	var direction = (goal_pos - global_position).normalized()
 	# Apply our speed to the direction vector to get our movement vector
-	var movement = direction * current_speed
+	var movement = direction * speed
 	# Move our KinematicBody2D node
 	move_and_collide(movement)
